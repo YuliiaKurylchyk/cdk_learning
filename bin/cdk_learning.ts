287d5549738e51d46bb8e7673398cdf702ff886d
Yuliia Kurylchyk <juliakurylo18@gmail.com>
2023-01-12 18:28:30 +0200

    Add cdk config

---
+++ bin/cdk_learning.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkLearningStack } from '../lib/cdk_learning-stack';
+
+const app = new cdk.App();
+
+new CdkLearningStack(app, 'CdkLearningStack', {
+    env: {
+        account: '702475694110',
+        region: 'us-east-1',
+
+    }
+});
+
+app.synth();

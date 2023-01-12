665d2a064d8fbf749815662377119ef2e1d4add7
Yuliia Kurylchyk <juliakurylo18@gmail.com>
2023-01-12 18:46:55 +0200

    fix

--- lib/cdk_learning-stack.ts
+++ lib/cdk_learning-stack.ts
@@ -6,7 +6,6 @@ export class CdkLearningStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
     new CodePipeline(this, 'Pipeline', {
         pipelineName: 'MyPipeline',
         synth: new ShellStep('Synth',{
